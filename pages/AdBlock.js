043451e5e9a22b3f768b376c3764f46006d9ede3
Sarwan Nizamani <sarwan.nizamani8@gmail.com>
2021-10-03 12:20:00 -0700

    some fixes

--- pages/AdBlock.js
+++ pages/AdBlock.js
@@ -1,29 +1,16 @@
 
-import styles from '../styles/Home.module.css'
-import { useState } from 'react'
+// import styles from '../styles/Home.module.css'
 
-export default function AdBlock() {
 
-    const defaultAdd={
-        detect:'Detecting....',
-        detecting:'Please Wait'
-    }
-    const [ state , setState ] = useState(defaultAdd) 
-    setTimeout(()=>{
-        setState({
-            detect:'ADD BLOCK DETECTED!',
-            detecting:'Please Refresh The page to continue!'
-        })
-    },5000)
-    return (
-        <div className={styles.container}>
-            <div className={styles.wrapper}>
-                <img className={styles.img} src="/pngegg" />
-                <p className={styles.detect}>{state.detect}</p>
-                <p className={styles.adp}>{state.detecting}</p>
-            </div>
-        </div>
+// export default function AdBlock() {
 
-    )
+   
+    
+//     return (
+       
 
-}
+
+
+//     )
+
+// }
